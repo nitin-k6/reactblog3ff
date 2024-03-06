1e241f044eb08908ea89c4596c33d740602fdc48
Nitin <67nitinkumar@gmail.com>
2024-03-06 19:03:00 +0530

    frontend

--- src/register/Register.jsx
+++ src/register/Register.jsx
@@ -13,7 +13,7 @@ export default function Login() {
     e.preventDefault();
     setError(false);
     try{
-      const res = await axios.post("https://reactblog3f.onrender.com//register", {
+      const res = await axios.post("https://reactblog3bb.onrender.com//register", {
         username,
         email,
         password,
