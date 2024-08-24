ea3d8c67b275c46688be1192fd8a193b5cb61154
Nitin Kumar <82951031+nitin-k6@users.noreply.github.com>
2024-08-24 22:45:12 +0530

    Update Home.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Header from "../../header/Header"
 import Posts from "../../posts/Posts"
-import Sidebar from "../../sidebar/Sidebar"
+// import Sidebar from "../../sidebar/Sidebar"
 import "./home.css"
 import axios from "axios";
 import { useLocation } from "react-router-dom";
@@ -50,7 +50,7 @@ useEffect(() => {
     <Header/>
     <div className="home">
     <Posts posts = {posts} />
-    <Sidebar/>
+{/*     <Sidebar/> */}
 
 
         </div>
